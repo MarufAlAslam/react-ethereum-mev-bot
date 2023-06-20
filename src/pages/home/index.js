76467f4b9b85f61317be948dbf299513d1933223
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-06-20 15:38:12 +0600

    tab added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/hero'
+import Info from '../../components/info'
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <Info/>
         </div>
     )
 }
